c40652b71591cd21dbd8f722b95bda27dae728ed
Haley <alicelai2920@gmail.com>
2019-12-23 11:11:06 -0800

    Initial Commit

---
+++ Binary_Search_Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Binary_Search_Tree
+//
+//  Created by Haley Lai on 12/23/19.
+//  Copyright © 2019 Haley Lai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
